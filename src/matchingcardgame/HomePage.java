8386c5b7f3ca8bd25e0909c3b27512488f3f5cfb
RNA <rainezali@gmail.com>
2025-10-15 17:51:27 +0300

    removing unnessary line

--- src/matchingcardgame/HomePage.java
+++ src/matchingcardgame/HomePage.java
@@ -8,7 +8,6 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
-import java.awt.Insets;
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JButton;
