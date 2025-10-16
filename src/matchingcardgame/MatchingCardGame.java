0ce21114da172c7a4e3b87b9817f02596bccd80b
RNA <rainezali@gmail.com>
2025-10-16 15:59:06 +0300

    Update MatchingCardGame.java

--- src/matchingcardgame/MatchingCardGame.java
+++ src/matchingcardgame/MatchingCardGame.java
@@ -12,20 +12,7 @@ public class MatchingCardGame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        HomePage hp = new HomePage();
-        hp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        hp.pack();
-        hp.setVisible(true);
         
-        UserInfo uip = new UserInfo();
-        uip.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        uip.pack();
-        uip.setVisible(true);
-        
-//        Test Testp = new Test();
-//        Testp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        Testp.pack();
-//        Testp.setVisible(true);
     }
     
 }
