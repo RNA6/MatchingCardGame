f510fc5f90dc6f2638ffde6de7b2729c83d771a7
RNA <rainezali@gmail.com>
2025-10-16 13:47:22 +0300

    update Theme Class
    Added some colors and font names

--- src/matchingcardgame/Theme.java
+++ src/matchingcardgame/Theme.java
@@ -6,9 +6,13 @@ package matchingcardgame;
 import java.awt.Color;
 
 public class Theme {
-    public static Color color_FF2DD1 =new Color(255, 45, 209);
-    public static Color color_CC66DA =new Color(204, 102, 218);
-    public static Color color_FDFFB8 =new Color(253, 255, 184);
-    public static Color color_63C8FF =new Color(99, 200, 255);
-    public static Color color_4DFFBE =new Color(77, 255, 190);
+    public static Color color_FF2DD1 = new Color(255, 45, 209);
+    public static Color color_CC66DA = new Color(204, 102, 218);
+    public static Color color_FDFFB8 = new Color(253, 255, 184);
+    public static Color color_63C8FF = new Color(99, 200, 255);
+    public static Color color_4DFFBE = new Color(77, 255, 190);
+    public static Color color_F3EFEF = new Color(243, 239, 239);
+    public static Color color_A19F9F = new Color(161, 159, 159);
+    public static String fontName1 = "Comic Sans MS";
+    public static String fontName2 = "Monospaced";
 }
