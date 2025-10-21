4558fbcc4d258df55699293b59cebe33a380b13c
RNA <rainezali@gmail.com>
2025-10-21 21:20:25 +0300

    fixing some problems
    removing unnecessary lines and adding some margin to stars

--- src/matchingcardgame/WinLose.java
+++ src/matchingcardgame/WinLose.java
@@ -2,24 +2,19 @@
 package matchingcardgame;
 
 
-import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
-
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
-import javax.swing.JButton;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class WinLose extends JFrame{
     
-     JPanel mainpanel = new JPanel(new GridLayout(2, 1, 0, 5));
+    JPanel mainpanel = new JPanel(new GridLayout(2, 1, 0, 5));
     JPanel iconpanel = new JPanel(new GridLayout(1, 3, 5, 0) );
-    
-    
     
     JLabel star1 = new JLabel();
     JLabel star2 = new JLabel();
@@ -29,38 +24,37 @@ public class WinLose extends JFrame{
     JLabel lose = new JLabel("YOU LOSE", JLabel.CENTER);
     
     public WinLose(){
-    super("win lose");
-    mainpanel.setBackground(Theme.color_FDFFB8);
-    iconpanel.setBackground(Theme.color_FDFFB8);
-    
-    setBounds(300, 130, 0, 0);
-    setMinimumSize(new Dimension(700,500));
-    setResizable(false);
-    // put images
-    star1.setIcon(new ImageIcon(getClass().getResource("normal-star.png")));
-    star2.setIcon(new ImageIcon(getClass().getResource("normal-star.png")));
-    star3.setIcon(new ImageIcon(getClass().getResource("dark-star.png")));
+        super("win lose");
+        getContentPane().setBackground(Theme.color_FDFFB8);
+        mainpanel.setOpaque(false);
+        iconpanel.setOpaque(false);
 
+        setBounds(300, 130, 0, 0);
+        setMinimumSize(new Dimension(700,500));
+        setResizable(false);
+        // put images
+        star1.setIcon(new ImageIcon(getClass().getResource("normal-star.png")));
+        star2.setIcon(new ImageIcon(getClass().getResource("normal-star.png")));
+        star3.setIcon(new ImageIcon(getClass().getResource("dark-star.png")));
+
+        star1.setSize(80, 80);
+        star2.setSize(120, 120);
+        star3.setSize(80, 80);
         
-    star1.setSize(80, 80);
-    star2.setSize(120, 120);
-    star3.setSize(80, 80);
-    
-    
-    iconpanel.add(star1);
-    iconpanel.add(star2);
-    iconpanel.add(star3);
-    win.setFont(new Font(Theme.fontName1, Font.BOLD, 40));
-    win.setForeground(new Color(255, 51, 170));
-    
-    lose.setFont(new Font(Theme.fontName1, Font.BOLD, 40));
-    win.setForeground(new Color(120, 40, 150));
-    
-    
-    mainpanel.add(iconpanel);
-    mainpanel.add(win);
-     
-    add(mainpanel);
+        iconpanel.setBorder(BorderFactory.createEmptyBorder(0, 100, 0, 0));
+        iconpanel.add(star1);
+        iconpanel.add(star2);
+        iconpanel.add(star3);
+        win.setFont(new Font(Theme.fontName1, Font.BOLD, 40));
+        win.setForeground(Theme.color_FF2DD1);
+
+        lose.setFont(new Font(Theme.fontName1, Font.BOLD, 40));
+        lose.setForeground(Theme.color_FF2DD1);
+
+        mainpanel.add(iconpanel);
+        mainpanel.add(win);
+
+        add(mainpanel);
     }
     
 }
