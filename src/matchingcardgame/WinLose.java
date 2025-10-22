fa2e4a3d4cd0edc4f54f82da5aa5d5144e0ec651
fatmah-abdullah <alrasasaifatmah@gmail.com>
2025-10-22 19:42:18 +0300

    Update WinLose.java (#39)

--- src/matchingcardgame/WinLose.java
+++ src/matchingcardgame/WinLose.java
@@ -1,37 +1,47 @@
-
 package matchingcardgame;
-
 
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.GridLayout;
+import java.awt.FlowLayout;
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class WinLose extends JFrame{
+
+public class WinLose extends JFrame {
     
-    JPanel mainpanel = new JPanel(new GridLayout(2, 1, 0, 5));
-    JPanel iconpanel = new JPanel(new GridLayout(1, 3, 5, 0) );
-    
+    // both panels now use FlowLayout
+    JPanel mainpanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+    JPanel iconpanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+    JPanel btnpanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+
     JLabel star1 = new JLabel();
     JLabel star2 = new JLabel();
     JLabel star3 = new JLabel();
-    
+
     JLabel win = new JLabel("YOU WIN", JLabel.CENTER);
     JLabel lose = new JLabel("YOU LOSE", JLabel.CENTER);
+    JLabel reward = new JLabel("YOUR REWARD IS #### POINTS", JLabel.CENTER);
+    JLabel tryagain = new JLabel("LETs TRY AGAIN", JLabel.CENTER);
     
-    public WinLose(){
+    JButton next = new JButton("NEXT");
+    JButton home = new JButton("HOME");
+    
+
+    public WinLose() {
         super("win lose");
         getContentPane().setBackground(Theme.color_FDFFB8);
         mainpanel.setOpaque(false);
         iconpanel.setOpaque(false);
+        btnpanel.setOpaque(false);
 
         setBounds(300, 130, 0, 0);
-        setMinimumSize(new Dimension(700,500));
+        setMinimumSize(new Dimension(700, 500));
         setResizable(false);
+
         // put images
         star1.setIcon(new ImageIcon(getClass().getResource("normal-star.png")));
         star2.setIcon(new ImageIcon(getClass().getResource("normal-star.png")));
@@ -40,23 +50,62 @@ public class WinLose extends JFrame{
         star1.setSize(80, 80);
         star2.setSize(120, 120);
         star3.setSize(80, 80);
-        
-        iconpanel.setBorder(BorderFactory.createEmptyBorder(0, 100, 0, 0));
+
+       
         iconpanel.add(star1);
         iconpanel.add(star2);
         iconpanel.add(star3);
+
+        // label styles
         win.setFont(new Font(Theme.fontName1, Font.BOLD, 40));
         win.setForeground(Theme.color_FF2DD1);
 
         lose.setFont(new Font(Theme.fontName1, Font.BOLD, 40));
         lose.setForeground(Theme.color_FF2DD1);
+        
+        reward.setFont(new Font(Theme.fontName1, Font.BOLD, 36));
+        reward.setForeground(Theme.color_FF2DD1);
+        
+        tryagain.setFont(new Font(Theme.fontName1, Font.BOLD, 36));
+        tryagain.setForeground(Theme.color_FF2DD1);
+        
+        //btn style
+        home.setBackground(Theme.color_63C8FF);
+        home.setFont(new Font(Theme.fontName2, Font.BOLD, 18));
+        home.setFocusPainted(false);
+        
+        next.setBackground(Theme.color_4DFFBE);
+        next.setFont(new Font(Theme.fontName2, Font.BOLD, 18));
+        next.setFocusPainted(false);
+        
+        //add buttons to panel
+        btnpanel.add(home);
+        btnpanel.add(next);
+        
+        
+        
+        // SET PANELS TO SET POSITIONS
+        
+        tryagain.setPreferredSize(new Dimension(400, 100));
+        tryagain.setBorder(BorderFactory.createEmptyBorder(30, 50, 0, 50));
+        
+        win.setPreferredSize(new Dimension(400, 100));
+        win.setBorder(BorderFactory.createEmptyBorder(5, 5, 0, 5));
+        
+        iconpanel.setPreferredSize(new Dimension(400, 150));
+        iconpanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 0, 5));
+        
+        btnpanel.setPreferredSize(new Dimension(500, 500));
+        btnpanel.setBorder(BorderFactory.createEmptyBorder(50, 00, 50, 50));
 
-        mainpanel.add(iconpanel);
+        // add components (same order as before)
         mainpanel.add(win);
+        mainpanel.add(iconpanel);
+        mainpanel.add(reward);
+        mainpanel.add(btnpanel);
+        // You can switch to "lose" instead by commenting the line above:
+        // mainpanel.add(lose);
 
         add(mainpanel);
     }
-    
 }
-
-
