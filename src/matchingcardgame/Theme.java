d0473c241afeb063f03cf0ad7068f4386edabfaa
RNA <133764521+RNA6@users.noreply.github.com>
2025-11-09 21:40:57 +0300

    adding comments to colors (#50)

--- src/matchingcardgame/Theme.java
+++ src/matchingcardgame/Theme.java
@@ -8,13 +8,14 @@ import java.awt.Dimension;
 import javax.swing.JFrame;
 
 public class Theme {
-    public static Color color_FF2DD1 = new Color(255, 45, 209);
-    public static Color color_CC66DA = new Color(204, 102, 218);
-    public static Color color_FDFFB8 = new Color(253, 255, 184);
-    public static Color color_63C8FF = new Color(99, 200, 255);
-    public static Color color_4DFFBE = new Color(77, 255, 190);
-    public static Color color_F3EFEF = new Color(243, 239, 239);
-    public static Color color_A19F9F = new Color(161, 159, 159);
+    public static Color color_FF2DD1 = new Color(255, 45, 209);//Pink
+    public static Color color_CC66DA = new Color(204, 102, 218);//Purple
+    public static Color color_FDFFB8 = new Color(253, 255, 184);//Yellow
+    public static Color color_63C8FF = new Color(99, 200, 255);//Blue
+    public static Color color_C2EAFF = new Color(194, 234, 255);//Light Blue
+    public static Color color_4DFFBE = new Color(77, 255, 190);//Green
+    public static Color color_F3EFEF = new Color(243, 239, 239);//Light Gray
+    public static Color color_A19F9F = new Color(161, 159, 159);//Gray
     public static String fontName1 = "Comic Sans MS";
     public static String fontName2 = "Monospaced";
     
