88a5bb8ed33eb8fc1c2c5174369c2bb6caed7be6
RNA <rainezali@gmail.com>
2025-11-10 22:25:55 +0300

    handling events in user menu panel
    Also making the user icon label common in UIComponents

--- src/matchingcardgame/UserMenuPanel.java
+++ src/matchingcardgame/UserMenuPanel.java
@@ -6,6 +6,8 @@ package matchingcardgame;
 
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -14,10 +16,11 @@ import javax.swing.JPanel;
  *
  * @author RNA
  */
-public class UserMenuPanel extends JPanel{
+public class UserMenuPanel extends JPanel implements ActionListener{
     
     private JButton userInfo_button;
     private JButton signOut_button;
+    private boolean mouseClicked = false;
     
     public UserMenuPanel(JFrame originalFrame){
         setLayout(new FlowLayout(FlowLayout.CENTER, 0, 10));
@@ -25,14 +28,23 @@ public class UserMenuPanel extends JPanel{
         setBackground(UITheme.color_CC66DA);
         
         userInfo_button = createUser_menuItem("User Info");
+        userInfo_button.addActionListener(this);
         UIUtilities.addNavigation(userInfo_button, originalFrame, new UserInfo(originalFrame));
         add(userInfo_button);
         
         signOut_button = createUser_menuItem("Sign Out");
+        signOut_button.addActionListener(this);
         UIUtilities.addNavigation(signOut_button, originalFrame, new UserInfo(originalFrame));
         add(signOut_button);
-        
         setVisible(false);
+    }
+    
+    public boolean isMouseClicked(){
+        return mouseClicked;
+    }
+    
+    public void updateMouseClicked(){
+        mouseClicked = !mouseClicked;
     }
     
     private JButton createUser_menuItem(String label){
@@ -41,5 +53,11 @@ public class UserMenuPanel extends JPanel{
         button.setBackground(UITheme.color_E29EEB);
         button.setPreferredSize(new Dimension(100, 40));
         return(button);
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        setVisible(false);
+        mouseClicked = false;
     }
 }
